629a3dda0120131d81ba3f8db68b48d026774d81
jltimmer <jltimmer@users.noreply.github.com>
2022-04-05 06:19:36 -0400

    Search user feature now routes correctly

--- app/components/stats-search.js
+++ app/components/stats-search.js
@@ -1,29 +1,15 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 // import ENV from '../../config/environment';
 
 export default class StatsSearchComponent extends Component {
   @tracked username;
+  @service router;
 
   @action
-  async search() {
-    let apiKey = "";
-    // e.preventDefault();
-    // console.log(ENV[TEST_KEY]);
-    // fetch(`https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=${username.value}&auth=${apiKey}`)
-    //   .then(response => response.json())
-    //   .then(data => console.log(data));
-    // console.log({username: username.value, thing: this});
-    const response = await fetch(
-      `https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=${username.value}&auth=${apiKey}`
-    );
-    const playerStats = await response.json();
-    
-    store.createRecord('post', {
-      username: playerStats.name,
-      level: playerStats.level
-    });
-    console.log(playerStats);
+  search() {
+    this.router.transitionTo('player-stats', { username: username.value} );
   }
 }
--- app/routes/player-stats.js
+++ app/routes/player-stats.js
@@ -1,14 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default class PlayerStatsRoute extends Route {
-  model(params) {
-    // let apiKey = "";
-    // const response = await fetch(`https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=${username}&auth=${apiKey}`);
-    // const playerStats = await response.json();
-    // console.log(ENV[TEST_KEY]);
-    // fetch(`https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=${username}&auth=${apiKey}`)
-    //   .then(response => response.json())
-    //   .then(data => console.log(data));
-    // return playerStats;
+  async model(params) {
+    let apiKey = "";
+    const response = await fetch(`https://api.mozambiquehe.re/bridge?version=5&platform=PC&player=${params.username}&auth=${apiKey}`);
+    const playerStats = await response.json();
+    console.log(playerStats);
+    return playerStats;
   }
 }
